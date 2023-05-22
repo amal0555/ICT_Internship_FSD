09e65156092076f9a5771acbe3d5f07145a71dfa
amal0555 <134275815+amal0555@users.noreply.github.com>
2023-05-22 15:09:42 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,5 @@
+let fruits = ["apple","grapes","orange",11];
+fruits.push("banana")
+fruits.pop()
+fruits.shift("banana")
+console.log(fruits);
---
+++ arrayobject.js
@@ -0,0 +1,11 @@
+let classess=[{
+    name:"AJ",
+    age:20,
+    place:"TDPA"
+},
+{
+    name:"JF",
+    age:20,
+    place:"EKM"
+}];
+console.log(classess[1].name);
